0f59cd7c2d4c47b5164b0379254faae6f7ef0c31
Harshit Bansal <128420207+harshit-1718@users.noreply.github.com>
2023-08-08 18:07:27 +0530

    Spot.py

--- Spot.py
+++ Spot.py
@@ -2,7 +2,7 @@ import pygame
 
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("A* Path Finding Algorithm")
+pygame.display.set_caption("Path Finding Algorithm")
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
